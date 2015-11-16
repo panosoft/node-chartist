cc5a3ad52a5681da8f8e5558cbc93caaea343fb3
Alexandre Gigliotti <alex@panosoft.com>
2015-11-16 11:50:59 -0800

    Add tests.

--- test/index.js
+++ test/index.js
@@ -1,9 +1,113 @@
+var cheerio = require('cheerio');
 var co = require('co');
+var expect = require('chai')
+  .use(require('chai-as-promised'))
+  .expect;
 var generate = require('../lib');
 var fs = require('fs');
 var path = require('path');
 
-co(function * () {
+var exists = path => new Promise((resolve, reject) => {
+  fs.stat(path, (error) => {
+    if (!error) resolve(true);
+    else if (error.code === 'ENOENT') resolve(false);
+    else reject(error);
+  });
+});
+
+describe('build', () => {
+  it('create dist/main.css', () => {
+    var filename = path.resolve(__dirname, '../dist/main.css');
+    return expect(exists(filename)).to.eventually.be.true;
+  });
+});
+describe('install', () => {
+  it('create node_modules/canvas.js', () => {
+    var filename = path.resolve(__dirname, '../node_modules/canvas.js');
+    return expect(exists(filename)).to.eventually.be.true;
+  });
+});
+describe('generate', () => {
+  describe('chart', () => {
+    // bar
+    it('bar', () => co(function * () {
+      var chart = yield generate('bar', {}, {
+        labels: ['a', 'b', 'c'],
+        series: [[1, 2, 3]]
+      });
+      var $ = cheerio.load(chart);
+      expect($('svg.ct-chart-bar').length).to.equal(1);
+    }));
+    it('line', () => co(function * () {
+      var chart = yield generate('line', {}, {
+        labels: ['a', 'b', 'c'],
+        series: [[1, 2, 3]]
+      });
+      var $ = cheerio.load(chart);
+      expect($('svg.ct-chart-line').length).to.equal(1);
+    }));
+    it('pie', () => co(function * () {
+      var chart = yield generate('pie', {}, {
+        series: [1, 2, 3]
+      });
+      var $ = cheerio.load(chart);
+      expect($('svg.ct-chart-pie').length).to.equal(1);
+    }));
+    it('throw on unsupported type', () =>
+      expect(generate('spider', {}, {})).to.eventually.be.rejectedWith(TypeError, /Unsupported chart type/)
+    );
+  });
+  describe('axisTitle', () => {
+    it('x axis', () => co(function * () {
+      var title = 'X Axis (units)';
+      var chart = yield generate('bar', {
+        axisX: { title }
+      }, {
+        labels: ['a', 'b', 'c'],
+        series: [[1, 2, 3]]
+      });
+      var $ = cheerio.load(chart);
+      expect($('text.ct-axis-title').length).to.equal(1);
+    }));
+    it('y axis', () => co(function * () {
+      var title = 'Y Axis (units)';
+      var chart = yield generate('bar', {
+        axisY: { title }
+      }, {
+        labels: ['a', 'b', 'c'],
+        series: [[1, 2, 3]]
+      });
+      var $ = cheerio.load(chart);
+      expect($('text.ct-axis-title').length).to.equal(1);
+    }));
+  });
+  describe('legend', () => {
+    it('generated', () => co(function * () {
+      var name = 'Y Axis (units)';
+      var chart = yield generate('bar', {}, {
+        labels: ['a', 'b', 'c'],
+        series: [{ name, value: [1, 2, 3] }]
+      });
+      var $ = cheerio.load(chart);
+      expect($('.ct-legend .ct-variable').length).to.equal(1);
+    }));
+    it('variable excluded if nameless', () => co(function * () {
+      var name = 'Y Axis (units)';
+      var chart = yield generate('bar', {}, {
+        labels: ['a', 'b', 'c'],
+        series: [
+          { name, value: [1, 2, 3] },
+          [1, 2, 3],
+          { name, value: [1, 2, 3] }
+        ]
+      });
+      var $ = cheerio.load(chart);
+      expect($('.ct-legend .ct-variable').length).to.equal(2);
+    }));
+  });
+});
+
+describe('rendered output', () => co(function * () {
   try {
     var options = {
       width: 400,
@@ -48,4 +152,4 @@ co(function * () {
     console.log(`${style}${bar}${line}${pie}`);
   }
   catch (error) { console.error(error.stack); }
-});
+}));
